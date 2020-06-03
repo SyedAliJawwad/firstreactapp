5d16df6847d120095a4f6f71dedd150bb3d8de9d
SyedAliJawwad <59028900+SyedAliJawwad@users.noreply.github.com>
2020-06-04 00:15:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> First React App auto deployed on surge using github workflow! </h1>
+      <h1> First React App auto deployed on surge using github workflow!! </h1>
     </div>
   );
 }
